e8ee6c592d384e79cb666a9e297ce6c380d7bfd8
jahidanowar <milon5143@gmail.com>
2020-03-12 20:57:32 +0530

    Layouts and Core Function Separate

---
+++ lib/screens/help.dart
@@ -0,0 +1,124 @@
+import 'package:flutter/material.dart';
+
+class HelpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Helps"),
+        centerTitle: true,
+        actions: <Widget>[],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            "assets/img/bg.png",
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.all(20.0),
+            child: ListView(
+              children: <Widget>[
+                Text("Direct Chat helps you to Message anyone on the WhatsApp without Saving their Contact  Number. It also helps you to generate personalized WhatsApp link with a predefined message and share it with your audience on your Social Networks!",
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    height: 1.8,
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  "How to use Direct Chat?",
+                  style: TextStyle(
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  "Step 1:",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text("Enter phone number in international format. Make sure that you don’t include any other characters in the phone number, only numeric/integer values.",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    height: 1.8,
+                  ),
+                ),
+                Text("Here is an example:",style: TextStyle(
+                    fontSize: 15.0,
+                    height: 1.8,
+                  ),),
+                SizedBox(height: 10.0,),
+                Text("My Phone Number: 076 848 3016",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(height: 8.0,),
+                Text("My Country Code: +91",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(height: 8.0,),
+                Text("I have to Enter the Number as: 91768483016",
+                  style: TextStyle(
+                    color: Colors.teal,
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  "Step 2:",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text("Write Your Message in the Message Field. You can type All types of Character. Maximum 250 Character allowed in this field. If you want to add more character You can do it from Whatsapp Message Field.",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    height: 1.8
+                  ),
+                ),
+                SizedBox(height: 20.0,),
+                Text(
+                  "Step 3:",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.w700
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text("If You want to send the message Directly then click the \"Send Message\" Button. If you want to copy the message link click on the Copy Button on the Bottom Right Corner. You can use this message link in Facebook or Twitter page. If some one click this link they can directly message the given number without saving their Phone Number.",
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    height: 1.8
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,220 @@
+import 'package:directwp/screens/about.dart';
+import 'package:directwp/screens/help.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  //Global Key TO Access Form State
+  final formKey = GlobalKey<FormState>();
+
+  //Global Key to Access Scafold State
+
+  final scafoldKey = GlobalKey<ScaffoldState>();
+
+  //Form Data
+  String phoneNumber;
+  String message;
+
+  // Function to send message
+  _sendMessage() async {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      // String wpUrl = "https://api.whatsapp.com/send?phone=$phoneNumber&text=$message";
+      String wpUrl = "https://wa.me/$phoneNumber?text=$message";
+
+      if (await canLaunch(wpUrl)) {
+        await launch(wpUrl);
+        print(wpUrl);
+      } else {
+        throw "Coudn't Launch the $wpUrl";
+      }
+    }
+  }
+
+  // FUnction to Copy Chat Link
+  _copyLink() {
+    if (formKey.currentState.validate()) {
+      formKey.currentState.save();
+      String wpUrl =
+          "https://api.whatsapp.com/send?phone=$phoneNumber&text=$message";
+      Clipboard.setData(ClipboardData(text: wpUrl));
+      final snackBar = SnackBar(
+        content: Text('Whatsapp chat link copied to clipboard'),
+      );
+      // Find the Scaffold in the widget tree and use
+      // it to show a SnackBar.
+      scafoldKey.currentState.showSnackBar(snackBar);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scafoldKey,
+      appBar: AppBar(
+        title: Text(widget.title),
+        centerTitle: true,
+        actions: <Widget>[
+          helpActionButton(),
+          aboutActionButton(),
+        ],
+      ),
+      body: Stack(
+        children: <Widget>[
+          Image.asset(
+            "assets/img/bg.png",
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Container(
+              margin: const EdgeInsets.all(30.0),
+              child: Form(
+                key: formKey,
+                child: ListView(
+                  children: <Widget>[
+                    logo(),
+                    SizedBox(height: 30.0),
+                    myLabel("Phone Number"),
+                    phoneNumberField(),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    myLabel("Message"),
+                    messageField(),
+                    SizedBox(height: 20),
+                    submitButton(),
+                    SizedBox(height: 30),
+                    // credits(),
+                  ],
+                ),
+              )),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _copyLink,
+        tooltip: 'Copy Chat Link',
+        child: Icon(Icons.content_copy),
+      ),
+    );
+  }
+
+//Widgets
+
+  Widget helpActionButton() {
+    return IconButton(
+      icon: Icon(Icons.help_outline),
+      tooltip: "Helps",
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => HelpScreen()));
+      },
+    );
+  }
+
+  Widget aboutActionButton() {
+    return IconButton(
+      icon: Icon(Icons.more_vert),
+      tooltip: "About",
+      onPressed: () {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => AboutScreen()));
+      },
+    );
+  }
+
+  Widget logo() {
+    return Image.asset(
+      "assets/img/logo-outlined.png",
+      width: 120,
+      height: 120,
+    );
+  }
+
+  Widget myLabel(String myLabelText) {
+    return Align(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        myLabelText,
+        style: TextStyle(
+          fontSize: 15,
+        ),
+      ),
+    );
+  }
+
+  Widget phoneNumberField() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+          // labelText: "Phone No. with Country Code",
+          hintText: "916295790000",
+          // border: OutlineInputBorder(),
+        ),
+        validator: (String value) {
+          if (value.isEmpty) {
+            return "Enter a Phone Number";
+          } else if (value.length != 12) {
+            return "Enter a Valid phone number with the country Code";
+          }
+          if (value.contains('+')) {
+            return "Remove the + sign";
+          }
+        },
+        onSaved: (String value) {
+          phoneNumber = value;
+        },
+      ),
+    );
+  }
+
+  Widget messageField() {
+    return Container(
+      margin: EdgeInsets.only(top: 10.0),
+      child: TextFormField(
+        maxLines: 3,
+        minLines: 3,
+        keyboardType: TextInputType.multiline,
+        decoration: InputDecoration(
+          // labelText: "Enter Your Message",
+          hintText: "Hi, How are you?",
+        ),
+        validator: (String value) {
+          //Return NULL if valid
+          // Otherwise String with Error Message
+          if (value.length >= 500) {
+            return "Maximum 100 Character Allowed";
+          }
+        },
+        onSaved: (String value) {
+          message = value;
+        },
+      ),
+    );
+  }
+
+  Widget submitButton() {
+    return SizedBox(
+      width: double.infinity,
+      child: RaisedButton(
+        onPressed: _sendMessage,
+        child: Text(
+          "Send Message",
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
