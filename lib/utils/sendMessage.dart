7d544ed9d7472311ef4aa075370c148f24222412
jahidanowar <milon5143@gmail.com>
2022-02-09 07:48:27 +0530

    fix: URL Launcher Bug fixed for android

--- lib/utils/sendMessage.dart
+++ lib/utils/sendMessage.dart
@@ -4,9 +4,10 @@ import 'package:url_launcher/url_launcher.dart';
 Future<bool> sendMessage({number, message}) async {
   number = trimWhiteSpaces(number);
   String wpUrl = "https://wa.me/$number?text=$message";
+  String encodedUrl = Uri.encodeFull(wpUrl);
   //Save data to database
-  if (await canLaunch(wpUrl)) {
-    return launch(wpUrl);
+  if (await canLaunch(encodedUrl)) {
+    return launch(encodedUrl);
   }
   return false;
 }
